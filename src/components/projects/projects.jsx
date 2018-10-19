8184cd608cf6df2dda597e08e48b350afaf26706
Nepein Andrey <nepein96@gmail.com>
2018-10-20 01:07:14 +0300

    Categories open button animation

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -10,18 +10,19 @@ export default class Projects extends Component {
   render() {
     return (
       <section className="projectsbox wrapper">
-          <CSSTransition in={ this.props.isCategoriesOpen } timeout={ 200 } classNames='projectsbox__filter'>
+          <CSSTransition in={ this.props.isCategoriesOpen } timeout={ 1000 } classNames='projectsbox__filter'>
               <div className="projectsbox__filter">
                   <h2>Категории</h2>
                   { this.props.getCats(this.props.categories) }
-                  <div className="projectsbox__filter-close" onClick={ this.props.toggleCategories }></div>
               </div>
           </CSSTransition>
           <div className="projectsbox__projects">
               <h1>Найдено 192 заказа</h1>
               { this.props.getProjects(this.props.projects) }
           </div>
-          <div className="projectsbox__filterbutton" onClick={ this.props.toggleCategories }>Категории</div>
+          <div className={ `projectsbox__filter-button ${(this.props.isCategoriesOpen) ? 'projectsbox__filter-button-active' : ''}` } onClick={ this.props.toggleCategories }>
+              { (this.props.isCategoriesOpen) ? '' : 'Категории' }
+          </div>
       </section>
       );
   }
--- src/containers/Projects.jsx
+++ src/containers/Projects.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 import * as Actions from '../actions';
 import Projects from '../components/projects/projects';
 import Project from '../components/projects/project';
+const getCats = categories => categories.map((val, i) => ( <a href="/" key={ i } className="category">
+                                                               { val }
+                                                           </a>));
 
-const getCats = categories => categories.map((val, i) => (
-  <a href="/" key={ i } className="category">
-      { val }
-  </a>
-));
-
-const getProjects = projects => projects.map((val, i) => (
-  <Project key={ i } val={ val } />
-));
+const getProjects = projects => projects.map((val, i) => ( <Project key={ i } val={ val } />));
 
 const mapState = state => ({
   projects: state.projectsReducer.projects,
@@ -26,5 +20,4 @@ const mapState = state => ({
 const mapDispatch = dispatch => ({
   toggleCategories: e => dispatch(Actions.toggleCategories(e))
 });
-
 export default connect(mapState, mapDispatch)(Projects);
